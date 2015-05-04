84de971d9f1b60221fe074ea44ca9b4385f9e036
mavericks <rajkumar.padilam@mavs.uta.edu>
2015-05-03 21:29:34 -0500

    Checked in Latest Code

---
+++ src/test.java
@@ -0,0 +1,11 @@
+
+public class test {
+	
+	public static void main(String[] args) {
+		
+		String asdas="this is raj krazzy";
+		String arr[]=asdas.split(" ");
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+	}
+}
